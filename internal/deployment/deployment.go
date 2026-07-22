6713e2b105db62d29639ae7b89be11b81719a887
Yevgeny Shnaidman <yshnaidm@redhat.com>
2026-07-22 15:32:10 +0300

    ImagePullPolicy for GC deployment  should honor the NodeFeatureDiscovery CR configuration

--- internal/deployment/deployment.go
+++ internal/deployment/deployment.go
@@ -119,7 +119,7 @@ func (d *deployment) SetGCDeploymentAsDesired(nfdInstance *nfdv1.NodeFeatureDisc
 					{
 						Name:            "nfd-gc",
 						Image:           nfdInstance.Spec.Operand.ImagePath(),
-						ImagePullPolicy: corev1.PullAlways,
+						ImagePullPolicy: getImagePullPolicy(nfdInstance),
 						Command: []string{
 							"nfd-gc",
 						},
